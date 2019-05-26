de739da62fa2cd6b514386bf95988aba4df06333
Piti Champeethong <piti@icstravelgroup.com>
2019-05-26 22:31:21 +0700

    create course material for hands-on

---
+++ 03-random-forests/rf.py
@@ -0,0 +1,63 @@
+#%%
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+# import warnings filter
+from warnings import simplefilter
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
+
+#%%
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split
+from scipy.io import arff
+import pandas as pd
+
+#%%
+data = arff.loadarff('dataset/data.arff')
+df = pd.DataFrame(data[0])
+str_df = df.select_dtypes([np.object]) 
+df = str_df.stack().str.decode('utf-8').unstack()
+list(df)
+df.head()
+
+#%%
+df = df.apply(lambda x: x.astype(str).str.lower())
+df = df.replace('yes', 1)
+df = df.replace('no', 0)
+df = df.replace('f', 1)
+df = df.replace('m', 0)
+
+#%%
+xVar = list(df.loc[:,'A1_Score':'A10_Score']) + ['gender'] + ['jundice'] + ['austim']
+print(df)
+yVar = df.iloc[:,18]
+df2 = df[xVar]
+
+#%%
+X_train, X_test, y_train, y_test = train_test_split(df2, yVar, test_size=0.2)
+print (X_train.shape, y_train.shape)
+print (X_test.shape, y_test.shape)
+
+#%%
+clf = RandomForestClassifier(n_jobs=2, random_state=0)
+
+clf.fit(X_train, y_train)
+RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None,
+            min_impurity_split=1e-07, min_samples_leaf=1,
+            min_samples_split=2, min_weight_fraction_leaf=0.0,
+            n_estimators=10, n_jobs=2, oob_score=False, random_state=0,
+            verbose=0, warm_start=False)
+
+#%%
+preds = clf.predict(X_test)
+
+#%%
+pd.crosstab(y_test, preds, rownames=['Actual Result'], colnames=['Predicted Result'])
+
+#%%
+list(zip(X_train, clf.feature_importances_))
---
+++ 07-Ensemble/ensemble.py
@@ -0,0 +1,101 @@
+#%%
+# import os
+# current_path = os.getcwd()
+# find = current_path.find('Ensemble')
+# if find == -1:
+#     os.chdir('./07-Ensemble/')
+# os.getcwd()
+
+#%%
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import MinMaxScaler
+
+#%%
+data = pd.read_csv('dataset/cancer.csv')
+data.head()
+
+#%%
+data.drop(['Sample code number'],axis = 1, inplace = True)
+
+#%%
+data.head()
+
+#%%
+data.describe()
+
+#%%
+data.info()
+
+#%%
+data['Bare Nuclei']
+
+#%%
+data.replace('?',0, inplace=True)
+data['Bare Nuclei']
+
+#%%
+# Convert the DataFrame object into NumPy array otherwise you will not be able to impute
+values = data.values
+
+# Now impute it
+imputer = Imputer()
+imputedData = imputer.fit_transform(values)
+
+#%%
+scaler = MinMaxScaler(feature_range=(0, 1))
+normalizedData = scaler.fit_transform(imputedData)
+
+#%%
+# Bagged Decision Trees for Classification - necessary dependencies
+
+from sklearn import model_selection
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+#%%
+# Segregate the features from the labels
+X = normalizedData[:,0:9]
+Y = normalizedData[:,9]
+
+#%%
+kfold = model_selection.KFold(n_splits=10, random_state=7)
+cart = DecisionTreeClassifier()
+num_trees = 100
+model = BaggingClassifier(base_estimator=cart, n_estimators=num_trees, random_state=7)
+results = model_selection.cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
+
+#%%
+# AdaBoost Classification
+
+from sklearn.ensemble import AdaBoostClassifier
+seed = 7
+num_trees = 70
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+model = AdaBoostClassifier(n_estimators=num_trees, random_state=seed)
+results = model_selection.cross_val_score(model, X, Y, cv=kfold)
+print(results.mean())
+
+#%%
+# Voting Ensemble for Classification
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+# create the sub models
+estimators = []
+model1 = LogisticRegression(solver='lbfgs')
+estimators.append(('logistic', model1))
+model2 = DecisionTreeClassifier()
+estimators.append(('cart', model2))
+model3 = SVC(gamma='scale')
+estimators.append(('svm', model3))
+# create the ensemble model
+ensemble = VotingClassifier(estimators)
+results = model_selection.cross_val_score(ensemble, X, Y, cv=kfold)
+print(results.mean())
